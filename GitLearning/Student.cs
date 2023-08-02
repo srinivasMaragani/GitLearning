0c3d392d91362ac30a398ea5f4a11492727ff099
Srinivas Maragani <srinivasaraomaragani1729@gmail.com>
2023-08-02 19:41:01 +0530

    adiexp

--- GitLearning/Student.cs
+++ GitLearning/Student.cs
@@ -1,11 +1,12 @@
 ﻿namespace GitLearning
 {
-	public class Student
+	public class Students
 	{
 		
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string StudentEmail { get; set;  }
 		public int StudentPhone { get; set; }
+		public string BadHabits { get; set; }
 	}
 }
