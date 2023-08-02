75abd87dc29768ce78f17e5f426588913cc0f6c9
Srinivas Maragani <srinivasaraomaragani1729@gmail.com>
2023-08-02 18:22:07 +0530

    something

--- GitLearning/Teacher.cs
+++ GitLearning/Teacher.cs
@@ -3,6 +3,7 @@
 	public class Teacher
 	{
 		public int Id { get; set; }
+		public string Name { get; set; }
 	                                                                                          
 	}
 }
